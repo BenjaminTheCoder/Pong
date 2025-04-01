4b00d39ba61b6a4870c7e0146f7707e3ec78dd44
BenjaminTheCoder <161790365+BenjaminTheCoder@users.noreply.github.com>
2025-03-31 20:21:58 -0700

    Physics work thank you to coding train for the edges method it was easy to translate into python.

--- pong.py
+++ pong.py
@@ -1,4 +1,6 @@
 import pygame as pg
+from dataclasses import dataclass
+
 pg.init()
 WINDOWHEIGHT = 600
 WINDOWWIDTH = 800
@@ -6,9 +8,36 @@ win = pg.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 
 pg.display.set_caption("Pong")
 
+@dataclass
+class Ball:
+    x: float
+    y: float
+    r: float
+    vx: float
+    vy: float
+
+    def edges(self) -> None:
+        if self.x >= WINDOWWIDTH - self.r:
+            self.x = WINDOWWIDTH - self.r
+            self.vx *= -1
+        elif self.x <= self.r:
+            self.x = self.r
+            self.vx *= -1
+
+        if self.y >= WINDOWHEIGHT - self.r:
+            self.y = WINDOWHEIGHT - self.r
+            self.vy *= -1
+        elif self.y <= self.r:
+            self.y = self.r
+            self.vy *= -1
+
+    def move(self) -> None:
+        self.x += self.vx
+        self.y += self.vy
+
+
 #variables player 1
 vel = 5
-
 
 p1 = pg.Rect(5, 255, 10, 150)
 
@@ -16,8 +45,10 @@ p1 = pg.Rect(5, 255, 10, 150)
 
 p2 = pg.Rect(785, 255, 10, 150)
 
+ball1 = Ball(400, 300, 50, 0.05, 0.05)
+ball2 = Ball(400, 300, 25, 0.05, 0.1)
 
-ball = pg.Rect(400, 300, 10, 10)
+
 bl = False
 br = False
 moveUpP1 = False
@@ -27,21 +58,25 @@ moveDownP2 = False
 bl = True
 run = True
 while run:
-    pg.time.delay(16)
-    if bl == True:
-        ball.x -= vel
-    if br == True:
-        ball.x += vel
-    if p1.colliderect(ball):
-        print("Yay!")
-        bl = False
-        br = True
-        ball.y += vel
-    if p2.colliderect(ball):
-        print("Wooh!")
-        bl = True
-        br = False
-        ball.y += vel
+    ball1.move()
+    ball1.edges()
+    ball2.move()
+    ball2.edges()
+    # pg.time.delay(16)
+    # if bl == True:
+    #     ball.x -= vel
+    # if br == True:
+    #     ball.x += vel
+    # if p1.colliderect(ball):
+    #     print("Yay!")
+    #     bl = False
+    #     br = True
+    #     ball.y += vel
+    # if p2.colliderect(ball):
+    #     print("Wooh!")
+    #     bl = True
+    #     br = False
+    #     ball.y += vel
 
 
     for event in pg.event.get():
@@ -76,8 +111,9 @@ while run:
         p2.y -= vel    
 
     win.fill((0, 0, 0))
-    pg.draw.rect(win, (255, 255, 255), p1)
-    pg.draw.rect(win, (255, 255, 255), p2)
-    pg.draw.rect(win, (255, 255, 255), ball)
+    #pg.draw.rect(win, (255, 255, 255), p1)
+    #pg.draw.rect(win, (255, 255, 255), p2)
+    pg.draw.circle(win, (255, 0, 0), (ball1.x, ball1.y), ball1.r)
+    pg.draw.circle(win, (0, 0, 255), (ball2.x, ball2.y), ball2.r)
     pg.display.update()
 pg.quit()
