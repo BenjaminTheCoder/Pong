2764dd9a4ebb0be114d135d8bf7fd6b52d0e0681
BenjaminTheCoder <161790365+BenjaminTheCoder@users.noreply.github.com>
2025-05-19 19:49:33 -0700

    Paddles on screen can hit ball kind of also go through floor.

--- ray_pong.py
+++ ray_pong.py
@@ -49,7 +49,6 @@ vel = 5
 
 p1 = pr.Rectangle(20, 255, 10, 100)
 
-#variables player 2
 
 p2 = pr.Rectangle(770, 255, 10, 100)
 
@@ -68,10 +67,51 @@ bl = True
 run = True
 
 while not pr.window_should_close():
+    #player 1
+    if pr.is_key_down(pr.KeyboardKey.KEY_W):
+        p1.y -= vel
+    if pr.is_key_down(pr.KeyboardKey.KEY_S):
+        p1.y += vel
+    if p1.y-p1.height<WINDOWHEIGHT:
+        p1.y += vel
+    if p1.y>0:
+        p1.y -= vel    
+    if pr.is_key_down(pr.KeyboardKey.KEY_A):
+        p1.x -= vel
+    if pr.is_key_down(pr.KeyboardKey.KEY_D):
+        p1.x += vel
+    if p1.x<0:
+       p1.x += vel
+    if p1.x+p1.width>400:
+       p1.x -= vel
+
+    if pr.is_key_down(pr.KeyboardKey.KEY_UP):
+        p2.y -= vel
+    if pr.is_key_down(pr.KeyboardKey.KEY_DOWN):
+        p2.y += vel
+    if p2.y-p2.height<WINDOWHEIGHT:
+        p2.y += vel
+    if p2.y+ p2.height>0:
+        p2.y -= vel    
+    if pr.is_key_down(pr.KeyboardKey.KEY_LEFT):
+        p2.x -= vel
+    if pr.is_key_down(pr.KeyboardKey.KEY_RIGHT):
+        p2.x += vel
+    if p2.x<0:
+       p2.x += vel
+    if p2.x<WINDOWWIDTH:
+       p2.x += vel
+    if p2.x+p2.width>419:
+       p2.x -= vel  
+
+
     ball1.move()
     ball1.colide()
+    ball1.colide_paddles(p1, p2)
     pr.begin_drawing()
     pr.clear_background(pr.BLACK)
-    pr.draw_circle( ball1.x, ball1.y, ball1.r, pr.WHITE)
+    pr.draw_circle_gradient( ball1.x, ball1.y, ball1.r, pr.WHITE, pr.GRAY)
+    pr.draw_rectangle(int(p1.x), int(p1.y), int(p1.width), int(p1.height), pr.WHITE)
+    pr.draw_rectangle(int(p2.x), int(p2.y), int(p2.width), int(p2.height), pr.WHITE)
     pr.end_drawing()
 pr.close_window()
